17711e1988e7da1eed854a4d0115bbd301e49c9d
Michael Hoisie <hoisie@gmail.com>
2010-03-05 08:26:48 -0800

    Added Tweet/User types. Follow now returns a channel.

--- twitterstream.go
+++ twitterstream.go
@@ -6,11 +6,59 @@ import (
     "encoding/base64"
     "http"
     "io"
+    "json"
     "os"
     "net"
     "strconv"
     "strings"
 )
+
+
+type User struct {
+    Lang                         string
+    Verified                     bool
+    Followers_count              int
+    Location                     string
+    Screen_name                  string
+    Following                    bool
+    Friends_count                int
+    Profile_background_color     string
+    Favourites_count             int
+    Description                  string
+    Notifications                string
+    Profile_text_color           string
+    Url                          string
+    Time_zone                    string
+    Statuses_count               int
+    Profile_link_color           string
+    Geo_enabled                  bool
+    Profile_background_image_url string
+    Protected                    bool
+    Contributors_enabled         bool
+    Profile_sidebar_fill_color   string
+    Name                         string
+    Profile_background_tile      string
+    Created_at                   string
+    Profile_image_url            string
+    Id                           int64
+    Utc_offset                   int
+    Profile_sidebar_border_color string
+}
+
+type Tweet struct {
+    Text                    string
+    Truncated               bool
+    Geo                     string
+    In_reply_to_screen_name string
+    Favorited               bool
+    Source                  string
+    Contributors            string
+    In_reply_to_status_id   string
+    In_reply_to_user_id     int64
+    Id                      int64
+    Created_at              string
+    User                    User
+}
 
 var filterUrl, _ = http.ParseURL("http://stream.twitter.com/1/statuses/filter.json")
 
@@ -18,6 +66,7 @@ type Client struct {
     Username string
     Password string
     conn     *http.ClientConn
+    tweets   chan Tweet
 }
 
 func encodedAuth(user, pwd string) string {
@@ -43,19 +92,23 @@ func (c *Client) readStream(resp *http.Response) {
             return
         }
         line = strings.TrimSpace(line)
-        if len(line) > 0 {
-            println(line)
-        } else {
-            println("nop")
+
+        if len(line) == 0 {
+            continue
         }
+
+        var tweet Tweet
+        json.Unmarshal(line, &tweet)
+
+        c.tweets <- tweet
     }
 }
 
 // Follow a list of user ids
-func (c *Client) Follow(ids []int64) os.Error {
+func (c *Client) Follow(ids []int64) (chan Tweet, os.Error) {
     conn, err := net.Dial("tcp", "", filterUrl.Host+":80")
     if err != nil {
-        return err
+        return nil, err
     }
     c.conn = http.NewClientConn(conn, nil)
 
@@ -76,21 +129,25 @@ func (c *Client) Follow(ids []int64) os.Error {
     req.ContentLength = int64(len(body.String()))
     if c.Username != "" {
         req.Header = map[string]string{
-            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Type":  "application/x-www-form-urlencoded",
             "Authorization": "Basic " + encodedAuth(c.Username, c.Password),
         }
     }
     err = c.conn.Write(&req)
     if err != nil {
-        return err
+        return nil, err
     }
 
     resp, err := c.conn.Read()
     if err != nil {
-        return err
+        return nil, err
+    }
+
+    if c.tweets == nil {
+        c.tweets = make(chan Tweet)
     }
 
     go c.readStream(resp)
 
-    return nil
+    return c.tweets, nil
 }
