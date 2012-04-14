30d4e1de5f7c2cc1ea0d2c1813e39844e9f0819f
Aaron Raddon <araddon@gmail.com>
2012-04-14 08:10:03 -0700

    add channel to shutdown stream, doc updates

--- examples/flowdock.go
+++ examples/flowdock.go
@@ -17,7 +17,7 @@ import (
 
 var (
 	token    *string = flag.String("token", "password", "Password")
-	flow     *string = flag.String("flow", "organization/flow", "Flowdock url path:  organization/flow")
+	flow     *string = flag.String("flow", "yourorg/flow", "Flowdock url path:  organization/flow")
 	logLevel *string = flag.String("logging", "debug", "Which log level: [debug,info,warn,error,fatal]")
 )
 
@@ -27,7 +27,9 @@ func main() {
 
 	// make a go channel for msgs
 	stream := make(chan []byte, 200)
-	flowUrl, _ := url.Parse("https://" + *token + "@stream.flowdock.com/flows" + *flow)
+	done := make(chan bool)
+
+	flowUrl, _ := url.Parse("https://" + *token + "@stream.flowdock.com/flows?filter=" + *flow)
 
 	// set the logger and log level
 	SetLogger(log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile), *logLevel)
@@ -36,7 +38,7 @@ func main() {
 	client := NewBasicAuthClient("", "", func(line []byte) {
 		stream <- line
 	})
-	_ = client.Connect(flowUrl, "")
+	_ = client.Connect(flowUrl, "", done)
 
 	for {
 		evt := <-stream
